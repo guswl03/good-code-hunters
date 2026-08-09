f1ad079d82ff90a165687bf973954e534cb0f07b
guswl03 <gee031022@gmail.com>
2026-08-09 11:48:10 +0900

    feat(export): add JSON export and independent regression fixtures

---
+++ goodcode/exporters/__init__.py
@@ -0,0 +1,13 @@
+"""Scan result exporters."""
+
+from goodcode.exporters.json_exporter import (
+    export_scan_result,
+    scan_result_to_dict,
+    scan_result_to_json,
+)
+
+__all__ = [
+    "export_scan_result",
+    "scan_result_to_dict",
+    "scan_result_to_json",
+]
